5a7419a8d56cc8d078402f527c38cb19d3080fd3
ilevi <itayill01@gmail.com>
2021-06-16 22:00:41 +0300

    Lower the color distance for brighter images

--- mosaic.py
+++ mosaic.py
@@ -23,6 +23,7 @@ def color_distance(color1, color2):
     """
     Good image score is about 50
     """
+    handicap = 50 if sum(chain(color1, color2)) > 200 * 6 else 0
     rdiff = color1[0] - color2[0]
     gdiff = color1[1] - color2[1]
     bdiff = color1[2] - color2[2]
